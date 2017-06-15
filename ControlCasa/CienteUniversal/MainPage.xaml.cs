99b4eccfe53152528425eae94c5313ea0b9c9bc8
Amin Espinoza <doomsday5@hotmail.com>
2017-06-14 21:10:25 -0500

    Commit 1
    Todo el proyecto terminado!!! Es hora de crear documentación!!

---
+++ ControlCasa/CienteUniversal/MainPage.xaml.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace CienteUniversal
+{
+    public sealed partial class MainPage : Page
+    {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "hubcasatulancingo.azure-devices.net";
+        static string deviceKey = "aJpjn3jlGDSlcFrOJqjCxXmHtm8HyBC5HTXr+TQ7pK0=";
+        static string deviceId = "maquinaCasa";
+
+        bool luzPolliPrendida = false;
+        bool luzOscarPrendida = false;
+        bool luzBanoAbjIntPrendida = false;
+        bool luzBanoAbjExtPrendida = false;
+        bool luzBanoArrIntPrendida = false;
+        bool luzBanoArrExtPrendida = false;
+        bool luzSalaPrendida = false;
+        bool luzCosasPrendida = false;
+        bool luzCocinaPrendida = false;
+        bool luzVentiladorPrendida = false;
+        bool luzTallerPrendida = false;
+        bool luzPatioPrendida = false;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            deviceClient = DeviceClient.Create(iotHubUri, AuthenticationMethodFactory.CreateAuthenticationWithRegistrySymmetricKey(deviceId, deviceKey), TransportType.Http1);
+
+        }
+
+        private void btnPolli_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("11", ref luzPolliPrendida);
+        }
+
+        private void btnOscar_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("04", ref luzOscarPrendida);
+        }
+
+        private void btnTaller_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("10", ref luzTallerPrendida);
+        }
+
+        private void btnBanoAbajoInt_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("01", ref luzBanoAbjIntPrendida);
+        }
+
+        private void btnBanoAbajoExt_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("06", ref luzBanoAbjExtPrendida);
+        }
+
+        private void btnCocina_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("08", ref luzCocinaPrendida);
+        }
+
+        private void btnSala_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("03", ref luzSalaPrendida);
+        }
+
+        private void btnPatio_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("12", ref luzPatioPrendida);
+        }
+
+        private void btnBanoArribaInt_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("02", ref luzBanoArrIntPrendida);
+        }
+
+        private void btnBanoArribaExt_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("05", ref luzBanoArrExtPrendida);
+        }
+
+        private void btnCuartoCosas_Click(object sender, RoutedEventArgs e)
+        {
+            HandleLightStatus("07", ref luzCosasPrendida);
+        }
+
+        private void btnVentilador_Click(object sender, RoutedEventArgs e)
+        {
+               HandleLightStatus("09", ref luzVentiladorPrendida);
+        }
+
+        private void HandleLightStatus(string light, ref bool handler)
+        {
+            if (handler)
+            {
+                EnviarInformaciónAlHub(light, "0");
+            }
+            else
+            {
+                EnviarInformaciónAlHub(light, "1");
+            }
+
+            handler = !handler;
+        }
+
+        private async void EnviarInformaciónAlHub(string light, string handler)
+        {
+            var telemetryDataPoint = new
+            {
+                lightNumber = light,
+                lightStatus = handler,
+                date = DateTime.Now
+            };
+
+            var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+            var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+            Debug.WriteLine(messageString);
+            await deviceClient.SendEventAsync(message);
+        }
+    }
+}
---
+++ ControlCasa/ControlCasa/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ControlCasa
+{
+    public partial class MainWindow : Window
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=hubcasatulancingo.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=kfSWlvhPZMFvWPs+oObC3bFAB5rlTUQ800Xo3lHGBoY=";
+        static string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+        EventHubReceiver eventHubReceiver;
+        DispatcherTimer timer;
+
+        bool luzUnoPrendida = false;
+        bool luzDosPrendida = false;
+        bool luzTresPrendida = false;
+        bool luzCuatroPrendida = false;
+        bool luzCincoPrendida = false;
+        bool luzSeisPrendida = false;
+        bool luzSietePrendida = false;
+        bool luzOchoPrendida = false;
+        bool luzNuevePrendida = false;
+        bool luzDiezPrendida = false;
+        bool luzOncePrendida = false;
+        bool luzDocePrendida = false;
+
+        static SerialPort puertoSerial = new SerialPort();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string portName = "COM4";
+            puertoSerial.PortName = portName;
+            puertoSerial.BaudRate = 9600;
+
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+            eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver("3", DateTime.UtcNow);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private async void Timer_Tick(object sender, EventArgs e)
+        {
+            await HandleReceivedInformation();
+        }
+
+        private async Task HandleReceivedInformation()
+        {
+            EventData eventData = await eventHubReceiver.ReceiveAsync();
+            string data = Encoding.UTF8.GetString(eventData.GetBytes());
+
+            JObject serializedObject = JObject.Parse(data);
+            string lightNumber = serializedObject["lightNumber"].ToString();
+            string lightStatus = serializedObject["lightStatus"].ToString();
+            DateTime lastMove = Convert.ToDateTime(serializedObject["date"]);
+
+            HandleLights(lightNumber, lightStatus);
+        }
+
+        private void HandleLights(string light, string status)
+        {
+            string finalCommand = string.Format("{0},{1}", light, status);
+
+            puertoSerial.Open();
+            puertoSerial.Write(finalCommand);
+            puertoSerial.Close();
+        }
+    }
+}
