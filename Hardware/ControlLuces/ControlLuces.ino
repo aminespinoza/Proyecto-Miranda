d733bfcd0591abb44250c801d5b9f2b4b612a15f
Amin Espinoza <doomsday5@hotmail.com>
2017-07-05 13:55:01 -0500

    Commit 11
    Corregidos los errores de las  instrucciones en Arduino.

--- Hardware/ControlLuces/ControlLuces.ino
+++ Hardware/ControlLuces/ControlLuces.ino
@@ -2,14 +2,14 @@ int relayOne = 2, redLedLightOne = 3, greenLedLightOne = 4;
 int relayTwo = 5, redLedLightTwo = 6, greenLedLightTwo = 7;
 int relayThree = 8, redLedLightThree = 9, greenLedLightThree = 10;
 int relayFour = 11, redLedLightFour = 12, greenLedLightFour = 13;
-int relayFive = 14, redLedLightFive = 15, greenLedLightFive = 16;
-int relaySix = 17, redLedLightSix = 18, greenLedLightSix = 19;
-int relaySeven = 20, redLedLightSeven = 21, greenLedLightSeven = 22;
-int relayEight = 23, redLedLightEight = 24, greenLedLightEight = 25;
-int relayNine = 26, redLedLightNine = 27, greenLedLightNine = 28;
-int relayTen = 29, redLedLightTen = 30, greenLedLightTen = 31;
-int relayEleven = 32, redLedLightEleven = 33, greenLedLightEleven = 34;
-int relayTwelve = 35, redLedLightTwelve = 36, greenLedLightTwelve = 37;
+int relayFive = 22, redLedLightFive = 23, greenLedLightFive = 24;
+int relaySix = 25, redLedLightSix = 26, greenLedLightSix = 27;
+int relaySeven = 28, redLedLightSeven = 29, greenLedLightSeven = 30;
+int relayEight = 31, redLedLightEight = 32, greenLedLightEight = 33;
+int relayNine = 34, redLedLightNine = 35, greenLedLightNine = 36;
+int relayTen = 37, redLedLightTen = 38, greenLedLightTen = 39;
+int relayEleven = 40, redLedLightEleven = 41, greenLedLightEleven = 42;
+int relayTwelve = 43, redLedLightTwelve = 44, greenLedLightTwelve = 45;
 
 void setup()
 {
@@ -60,6 +60,18 @@ char charBuf[4];
 
 void loop()
 {
+  digitalWrite(redLedLightOne, HIGH);
+  digitalWrite(redLedLightTwo, HIGH);
+  digitalWrite(redLedLightThree, HIGH);
+  digitalWrite(redLedLightFour, HIGH);
+  digitalWrite(redLedLightFive, HIGH);
+  digitalWrite(redLedLightSix, HIGH);
+  digitalWrite(redLedLightSeven, HIGH);
+  digitalWrite(redLedLightEight, HIGH);
+  digitalWrite(redLedLightNine, HIGH);
+  digitalWrite(redLedLightTen, HIGH);
+  digitalWrite(redLedLightEleven, HIGH);
+  digitalWrite(redLedLightTwelve, HIGH);
   if(Serial.available()>0)
   {
     data = Serial.readString();
@@ -75,48 +87,63 @@ void loop()
 void handleLightFromValue(char lightPosition, char lightSecondPosition, char lightValue)
 {
   String stringValue = String(lightPosition) + String(lightSecondPosition);
+  Serial.println(stringValue);
   int finalValue = stringValue.toInt();
+  Serial.println(finalValue);
   
   switch (finalValue)
   {
     case 1:
+    Serial.println("Baño abajo interior");
       ShowSpecificColor(lightValue, redLedLightOne, greenLedLightOne, relayOne);
       break;
     case 2:
+    Serial.println("Baño arriba interior");
       ShowSpecificColor(lightValue, redLedLightTwo, greenLedLightTwo, relayTwo);
       break;
     case 3:
+    Serial.println("Sala");
       ShowSpecificColor(lightValue, redLedLightThree, greenLedLightThree, relayThree);
       break;
     case 4:
+    Serial.println("Oscar");
       ShowSpecificColor(lightValue, redLedLightFour, greenLedLightFour, relayFour);
       break;
     case 5:
+    Serial.println("Baño arriba exterior");
       ShowSpecificColor(lightValue, redLedLightFive, greenLedLightFive, relayFive);
       break;
     case 6:
+    Serial.println("Baño abajo exterior");
       ShowSpecificColor(lightValue, redLedLightSix, greenLedLightSix, relaySix);
       break;
     case 7:
+    Serial.println("Cuarto cosas");
       ShowSpecificColor(lightValue, redLedLightSeven, greenLedLightSeven, relaySeven);
       break;
     case 8:
+    Serial.println("led ocho");
       ShowSpecificColor(lightValue, redLedLightEight, greenLedLightEight, relayEight);
       break;
     case 9:
+    Serial.println("led nueve");
       ShowSpecificColor(lightValue, redLedLightNine, greenLedLightNine, relayNine);
       break;
     case 10:
+    Serial.println("Taller");
       ShowSpecificColor(lightValue, redLedLightTen, greenLedLightTen, relayTen);
       break;
     case 11:
+    Serial.println("Polli");
       ShowSpecificColor(lightValue, redLedLightEleven, greenLedLightEleven, relayEleven);
       break;
     case 12:
+    Serial.println("Patio");
       ShowSpecificColor(lightValue, redLedLightTwelve, greenLedLightTwelve, relayTwelve);
       break;
     case 13:
       TriggerAlarm();
+      break;
     default:
     break;
   }
@@ -124,13 +151,13 @@ void handleLightFromValue(char lightPosition, char lightSecondPosition, char lig
 
 void ShowSpecificColor(int lightStatus, int redLight, int greenLight, int relayNumber)
 {
-  if(lightStatus == '1')
+  if(lightStatus == '0')
   {
     digitalWrite(relayNumber, LOW);
     digitalWrite(redLight, HIGH);
     digitalWrite(greenLight, LOW);
   }
-  else if(lightStatus == '0')
+  else if(lightStatus == '1')
   {
     digitalWrite(relayNumber, HIGH);
     digitalWrite(redLight, LOW);
@@ -140,7 +167,7 @@ void ShowSpecificColor(int lightStatus, int redLight, int greenLight, int relayN
 
 void TriggerAlarm()
 {
-  for(int i = 0; i < 5; i++ )
+  for(int i = 0; i < 8; i++ )
   {
     digitalWrite(relayOne, HIGH);
     digitalWrite(relayTwo, HIGH);
@@ -154,7 +181,7 @@ void TriggerAlarm()
     digitalWrite(relayTen, HIGH);
     digitalWrite(relayEleven, HIGH);
     digitalWrite(relayTwelve, HIGH);
-    delay(1000);
+    delay(500);
     digitalWrite(relayOne, LOW);
     digitalWrite(relayTwo, LOW);
     digitalWrite(relayThree, LOW);
@@ -167,7 +194,6 @@ void TriggerAlarm()
     digitalWrite(relayTen, LOW);
     digitalWrite(relayEleven, LOW);
     digitalWrite(relayTwelve, LOW);
-    delay(1000);
+    delay(500);
   }
 }
-
