d70d509e4ca6846c0d5cb704a2f51f3a6ff5c5fc
Amin Espinoza <doomsday5@hotmail.com>
2017-12-02 22:24:07 -0600

    Commit 15.1
    El botón de luces está ya actualizado para reflejar su cambio de estado.

---
+++ Software/ControlCasa/ControlCasaUniversal/Converters/LightConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace ControlCasaUniversal.Converters
+{
+    public class LightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool isDecided = (bool)value;
+
+            if (isDecided)
+            {
+                return new SolidColorBrush(Color.FromArgb(255, 15, 106, 158));
+            }
+            else
+            {
+                return new SolidColorBrush(Color.FromArgb(51, 15, 106, 158));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
